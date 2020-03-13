0d2e2aa5a6f81f75c8fe3af3a335b9c8d1fb6367
Chris Price <christopher.r.price@gmail.com>
2020-03-12 22:20:18 -0400

    Basic UI set up.

--- FindACrew/ViewControllers/PersonSearchTableViewController.swift
+++ FindACrew/ViewControllers/PersonSearchTableViewController.swift
@@ -9,15 +9,11 @@
 import UIKit
 
 class PersonSearchTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
