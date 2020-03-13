8cc7cd2507b1328eb4d15552880cbd89f7220ad6
Chris Price <christopher.r.price@gmail.com>
2020-03-12 21:57:08 -0400

    Basic file stubs

---
+++ FindACrew/Models/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  FindACrew
+//
+//  Created by Chris Price on 3/12/20.
+//  Copyright © 2020 BuildWeek1x2. All rights reserved.
+//
+
+import Foundation
---
+++ FindACrew/Views/PersonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonTableViewCell.swift
+//  FindACrew
+//
+//  Created by Chris Price on 3/12/20.
+//  Copyright © 2020 BuildWeek1x2. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
